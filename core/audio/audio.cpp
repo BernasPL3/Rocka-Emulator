9e38117f1a83843376853029bf932de0d4f0af56
BernasPL3 <bernardogamesguidi@gmail.com>
2026-05-24 11:52:58 -0300

    Create audio.cpp

---
+++ core/audio/audio.cpp
@@ -0,0 +1,10 @@
+#include "audio.h"
+#include <iostream>
+
+void Audio::Init() {
+    std::cout << "[Audio] Sistema iniciado\n";
+}
+
+void Audio::PlaySample() {
+    std::cout << "[Audio] Tocando sample\n";
+}
